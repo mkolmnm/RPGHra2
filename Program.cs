6d5be97a10daf205eec0b2d79c4ab6fbfd570774
mkolmnm <marek@mesaros.cz>
2026-03-30 10:59:30 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RPGHra2
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            string[] menuPrvni = { "Play", "Choose a Character", "Exit" };
+            int volbaPohyb = RPGHra2.Moznosti.VykresliMoznosti(menuPrvni, "Kam chceš jít?");
+            VypisVysledek("Choose one: ");
+        }
+
+        static void VypisVysledek(string text)
+        {
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine(" >> " + text + " << ");
+
+            Console.ResetColor();
+            Console.WriteLine("\n(Stiskni libovolnou klávesu pro pokračování...)");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ VykresliMoznosti.cs
@@ -0,0 +1,49 @@
+﻿namespace RPGHra2
+{
+
+
+    public class Moznosti
+    {
+
+        public static int VykresliMoznosti(string[] moznosti, string titulek)
+        {
+            int vybranyIndex = 0;
+            ConsoleKey klavesa;
+
+            Console.CursorVisible = false;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(new string('-', titulek.Length));
+                Console.WriteLine(titulek);
+
+                for (int i = 0; i < moznosti.Length; i++)
+                {
+                    if (i == vybranyIndex)
+                    {
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine($"> {moznosti[i]} <");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"  {moznosti[i]}  ");
+                    }
+                }
+
+                klavesa = Console.ReadKey(true).Key;
+
+                if (klavesa == ConsoleKey.UpArrow && vybranyIndex > 0)
+                    vybranyIndex--;
+                else if (klavesa == ConsoleKey.DownArrow && vybranyIndex < moznosti.Length - 1)
+                    vybranyIndex++;
+
+            } while (klavesa != ConsoleKey.Enter);
+
+            Console.CursorVisible = true;
+            return vybranyIndex; // Vrátí číslo vybrané položky
+        }
+    }
+}
