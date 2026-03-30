bee73a69068c4f902e941d8c1e85dec600fd0fa9
mkolmnm <marek@mesaros.cz>
2026-03-30 14:25:34 +0200

    vylepseni vzhledu mainu a switch
    pridani switche, vylepseni vzhledu a lepsi prehlednost

--- Program.cs
+++ Program.cs
@@ -9,20 +9,41 @@ namespace RPGHra2
         {
             string[] menuPrvni = { "Play", "Choose a Character", "Exit" };
             int volbaPohyb = RPGHra2.Moznosti.VykresliMoznosti(menuPrvni, "Kam chceš jít?");
-            VypisVysledek("Choose one: ");
+            switch (volbaPohyb)
+            {
+                case 0:
+                    //Vypsani hry
+                    while (true)
+                    {
+                        Console.WriteLine("Hra");
+                    }
+                    break;  
+                                        
+                case 1:
+                    //Vybrani charakteru
+                    while (true)
+                    {
+                        Console.WriteLine("Vyber charakter!");
+                    }
+                    
+                    break;
+                case 2:
+                    Console.WriteLine("Konec");
+                    break;
+            }
         }
-
-        static void VypisVysledek(string text)
+        public static void VypisZadaní(string text)
         {
             Console.WriteLine();
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.ForegroundColor = ConsoleColor.Black;
 
-            Console.WriteLine(" >> " + text + " << ");
+            Console.WriteLine($"| {text} |");
 
             Console.ResetColor();
             Console.WriteLine("\n(Stiskni libovolnou klávesu pro pokračování...)");
             Console.ReadKey(true);
         }
+    }   
+       
     }
-}
--- obj/Debug/net10.0/RPGHra2.AssemblyInfo.cs
+++ obj/Debug/net10.0/RPGHra2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RPGHra2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d5be97a10daf205eec0b2d79c4ab6fbfd570774")]
 [assembly: System.Reflection.AssemblyProductAttribute("RPGHra2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RPGHra2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
